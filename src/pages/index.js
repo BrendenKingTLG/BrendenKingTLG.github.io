67f38fcebab13f867c797673e4be3448af553647
BrendenKingTLG <kingbren@oregonstate.edu>
2023-04-03 14:01:50 -0400

    added footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
-import Head from 'next/head'
-
+import Head from "next/head";
+import {
+  SiTypescript,
+  SiPython,
+  SiFlask,
+  SiBootstrap,
+  SiLinux,
+  SiMongodb,
+  SiOpenjdk,
+  SiAmazonaws,
+  SiReact,
+  SiLinkedin,
+  SiGithub
+} from "react-icons/si";
 
 export default function Home() {
   return (
@@ -10,50 +22,121 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/development.svg" />
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
-        <link href="https://fonts.googleapis.com/css2?family=Anton&family=Noto+Color+Emoji&display=swap" rel="stylesheet"></link>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossorigin
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Anton&family=Noto+Color+Emoji&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
-      <main className=' min-h-screen bg-slate-50'>
-      <section className='' >
-          <nav className=' shadow-sm bg-slate-600' id='mainNav' >
-            <ul className='flex justify-center my-5'>
-              <li><a href="/" className='cursor-pointer'>Home</a></li>
-              <li><p>|</p></li>
-              <li><a href="/resume" className='cursor-pointer'>Resume</a></li>
-              <li><p>|</p></li>
-              <li><a href="/projects" className='cursor-pointer'>Projects</a></li>
+      <main className=" min-h-screen bg-white">
+        <section className="">
+          <nav className=" shadow-sm bg-slate-600" id="mainNav">
+            <ul className="flex justify-center my-5">
+              <li>
+                <a href="/" className="cursor-pointer">
+                  Home
+                </a>
+              </li>
+              <li>
+                <p>|</p>
+              </li>
+              <li>
+                <a href="#resume" className="cursor-pointer">
+                  Resume
+                </a>
+              </li>
+              <li>
+                <p>|</p>
+              </li>
+              <li>
+                <a href="#projects" className="cursor-pointer">
+                  Projects
+                </a>
+              </li>
             </ul>
           </nav>
-      </section>
-      <section className='px-6'>
-          <section>
-            <h2 className=' font-bold text-center text-5xl mb-4 pt-9'>
+        </section>
+        <section className="">
+          <section className=" bg-white" style={{height: "20em"}}>
+            <h2 className=" font-bold text-center text-5xl mb-4 pt-9">
               Brenden King
             </h2>
-            <h3 className='font-bold text-center text-xl'>Software Engineer, Developer and Designer</h3>
-            <p className='text-center px-3 py-2 mx-30 leading-7'>
-            My technical expertise includes proficiency in programming languages such as Java, Python, and JavaScript, as well as in-depth knowledge of database management systems, web development frameworks, and cloud computing platforms. I am also experienced in agile development methodologies, test-driven development, and continuous integration and delivery
+            <h3 className="font-bold text-center text-xl">
+              Software
+            </h3>
+            <p className="text-center px-3 py-2 mx-30 leading-7">
+              My technical expertise includes proficiency in programming
+              languages such as Java, Python, and JavaScript, as well as
+              in-depth knowledge of database management systems, web development
+              frameworks, and cloud computing platforms. I am also experienced
+              in agile development methodologies, test-driven development, and
+              continuous integration and delivery
             </p>
           </section>
-          <section className=''>
-            <h2 className='text-center font-bold text-xl m-5'>Tech Stack</h2>
-            <div className='flex justify-between'>
-              <img src="/java (1).svg" alt="java logo" id='java'/><img src="/aws.svg" alt="java logo"/><img src="/linux.svg" alt="java logo"/>
-            </div>
-            <div className='flex justify-between'>
-            <img src="/js.svg" alt="java logo"/><img src="/mongodb.svg" alt="java logo"/><img src="/python (1).svg" id='python' alt="java logo"/>
+          <section className="content-center bg-indigo-400 p-6" >
+            <h2 className="text-center font-bold text-4xl m-5">Tech Stack</h2>
+            <div className=" content-center" style={{margin: "auto", width: "80%"}}>
+              <h3 className="text-center font-bold text-xl m-5">Frontend</h3>
+              <div className=" flex justify-between" >
+                <SiTypescript size={50} style={{fill: "white"}}/>
+                <SiBootstrap size={50} style={{fill: "white"}}/>
+                <SiReact size={50} style={{fill: "white"}}/>
+              </div>
+              <h3 className="text-center font-bold text-xl m-5">Backend</h3>
+              <div className=" flex justify-between" >
+                <SiOpenjdk size={50} style={{fill: "white"}}/>
+                <SiFlask size={50} style={{fill: "white"}}/>
+                <SiMongodb size={50} style={{fill: "white"}}/>
+              </div>
+              <h3 className="text-center font-bold text-xl m-5">Systems</h3>
+              <div className=" flex justify-between" >
+                <SiAmazonaws size={50} style={{fill: "white"}}/>
+                <SiPython size={50} style={{fill: "white"}}/>
+                <SiLinux size={50} style={{fill: "white"}}/>
+              </div>
             </div>
           </section>
-          <section className=''>
-            <h2 className=' text-center font-bold text-xl m-5'>Connect With Me</h2>
-            <div className=' flex justify-center'>
-            <a href="https://www.linkedin.com/in/brenden-king-swe/" target={'_blank'} rel={'noopener'}><img src="/linkedin.svg" alt="java logo" className='mx-4'/></a><a target={'_blank'} rel={'noopener'} href="https://github.com/BrendenKingTLG"><img src="/github.svg" alt="java logo" className='mx-4'/></a>
+          <section className=" p-3" style={{height: "20em"}} >
+            <h2 className=" text-center font-bold text-5xl m-5">
+              Connect With Me
+            </h2>
+            <div className=" flex justify-between" style={{margin: "auto", width: "15%"}}>
+              <a
+                href="https://www.linkedin.com/in/brenden-king-swe/"
+                target={"_blank"}
+                rel={"noopener"}
+              >
+                <SiLinkedin size={50} />
+              </a>
+              <a
+                target={"_blank"}
+                rel={"noopener"}
+                href="https://github.com/BrendenKingTLG"
+              >
+                <SiGithub size={50} />
+              </a>
             </div>
           </section>
-          <section className=''>
+          <section id="resume">
+            <h2 className="text-6xl font-bold text-center m-3">Resume</h2>
+            <center className=" px-6">
+              <iframe
+                className=" min-w-full min-h-screen"
+                src="/Brenden D. King-Resume.pdf"
+                title="Resume"
+              ></iframe>
+            </center>
+          </section>
+          <section className="text-6xl font-bold text-center m-3" id="projects">
+            <h2>Projects</h2>
+            <h3 className="my-10">Coming Soon</h3>
           </section>
         </section>
       </main>
     </>
-  )
+  );
 }
