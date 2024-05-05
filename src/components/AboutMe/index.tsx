2a36d134187edb481fea40cfdea9e25b9c1d3bb5
BrendenKingTLG <kingbren@oregonstate.edu>
2024-05-05 17:16:15 -0500

    fixed linting errors

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -3,27 +3,28 @@ import React from "react";
 export default function BrendenIntro() {
   return (
     <div id="about">
-      <h2 className=" mt-10 text-center text-2xl font-bold mb-3">Summary</h2>
-      <div className=" flex content-center lg:mx-40">
+      <h2 className="mt-10 text-center text-2xl font-bold mb-3">Summary</h2>
+      <div className="flex content-center lg:mx-40">
         <div className="mockup-code bg-base-200 p-4 rounded-box">
           <pre data-prefix="$" className="">
             Tell me about yourself
           </pre>
           <br />
           <code className="text-base-content">
-            Hey there! I'm Brenden, a tech enthusiast with a knack for crafting
-            digital solutions that blend innovation with user-centric design.
-            With a Master's degree in Computer Science and a penchant for
-            tinkering with code, I thrive on the thrill of turning complex
-            problems into elegant solutions. Whether it's diving into the depths
-            of Java, dancing with JavaScript, or sculpting seamless user
-            experiences with React and Angular, I'm always up for the challenge.
-            My journey in technology has been a whirlwind of learning and
-            growth, marked by certifications in security, cloud architecture,
-            and more. When I'm not immersed in lines of code, you'll find me
-            exploring the latest tech trends, tinkering with gadgets, or losing
-            myself in a good book on software architecture. Let's connect and
-            embark on an exciting journey of innovation together!
+            Hey there! I&apos;m Brenden, a tech enthusiast with a knack for
+            crafting digital solutions that blend innovation with user-centric
+            design. With a Master&apos;s degree in Computer Science and a
+            penchant for tinkering with code, I thrive on the thrill of turning
+            complex problems into elegant solutions. Whether it&apos;s diving
+            into the depths of Java, dancing with JavaScript, or sculpting
+            seamless user experiences with React and Angular, I&apos;m always up
+            for the challenge. My journey in technology has been a whirlwind of
+            learning and growth, marked by certifications in security, cloud
+            architecture, and more. When I&apos;m not immersed in lines of code,
+            you&apos;ll find me exploring the latest tech trends, tinkering with
+            gadgets, or losing myself in a good book on software architecture.
+            Let&apos;s connect and embark on an exciting journey of innovation
+            together!
           </code>
         </div>
       </div>
