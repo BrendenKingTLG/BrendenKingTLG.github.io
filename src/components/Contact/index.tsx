ab3ddd6c818cf7e5fd4d475df05190ffcb983f04
BrendenKingTLG <kingbren@oregonstate.edu>
2024-05-11 15:48:25 -0500

    added mailer

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -34,38 +34,41 @@ export default function Index() {
   };
 
   return (
-    <div className="lg:mx-60 border-2 mt-5 p-5 shadow-lg rounded-lg">
-      <form onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Email:
-          </label>
-          <input
-            type="text"
-            name="from"
-            value={formData.from}
-            onChange={handleChange}
-            className="input input-bordered w-full"
-            placeholder="Your email address"
-          />
-        </div>
-        <div className="mb-6">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Message:
-          </label>
-          <textarea
-            name="textBody"
-            value={formData.textBody}
-            onChange={handleChange}
-            className="textarea textarea-bordered w-full min-h-[120px]"
-            placeholder="Write your message here"
-          ></textarea>
-        </div>
-        <button type="submit" className="btn btn-primary w-full">
-          Send Email
-        </button>
-      </form>
-      {message && <p className="mt-4">{message}</p>}
+    <div id="chat">
+      <h2 className="mt-10 text-center text-2xl font-bold mb-3">Contact</h2>
+      <div className="lg:mx-60 border-2 mt-5 p-5 shadow-lg rounded-lg">
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Email:
+            </label>
+            <input
+              type="text"
+              name="from"
+              value={formData.from}
+              onChange={handleChange}
+              className="input input-bordered w-full"
+              placeholder="Your email address"
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Message:
+            </label>
+            <textarea
+              name="textBody"
+              value={formData.textBody}
+              onChange={handleChange}
+              className="textarea textarea-bordered w-full min-h-[120px]"
+              placeholder="Write your message here"
+            ></textarea>
+          </div>
+          <button type="submit" className="btn btn-primary w-full">
+            Send Email
+          </button>
+        </form>
+        {message && <p className="mt-4">{message}</p>}
+      </div>
     </div>
   );
 }
