14996d0421acecc2961888acec8cc055affadb1e
BrendenKingTLG <kingbren@oregonstate.edu>
2024-10-25 20:23:29 -0500

    update tech stack cols

--- components/about-me.tsx
+++ components/about-me.tsx
@@ -42,7 +42,7 @@ export default function AboutMe() {
           Below are some of the technical skills I have experience with:
         </p>
 
-        <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-8">
+        <div className="grid gap-8 grid-cols-2 lg:grid-cols-3 mt-8">
           {skills.map((skill) => (
             <div
               key={skill.name}
